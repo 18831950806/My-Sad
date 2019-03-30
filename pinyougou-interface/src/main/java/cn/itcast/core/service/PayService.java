f96a555a5151daa4e77aa9b24f928530230c80ad
qiaomu <qiaomu@item.com>
2019-03-31 01:02:17 +0800

    azzz

--- pinyougou-interface/src/main/java/cn/itcast/core/service/PayService.java
+++
@@ -1,9 +0,0 @@
-package cn.itcast.core.service;
-
-import java.util.Map;
-
-public interface PayService {
-    Map<String,String> createNative(String name);
-
-    Map<String,String> queryPayStatus(String out_trade_no);
-}
